dafdd5218efa85e9c6743b39ba7c238d02b47b27
szasadny <szasadny@live.nl>
2023-05-26 16:47:31 +0200

    Vrijdag einde werktijd oopsie

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -46,7 +46,6 @@ public class BookService {
 	                throw new IllegalArgumentException("Invalid search field: " + searchField);
 	        }
 	    } else {
-	    	System.out.println("test");
 	        return repo.searchBooks(searchTerm.toLowerCase());
 	    }
 	}
